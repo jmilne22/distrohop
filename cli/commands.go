25bdd753785b1e7adbdce4d8628d549ce956f097
James Milne <jmilne22@gmail.com>
2025-11-08 23:31:25 +0200

    tidy

--- cli/commands.go
+++ cli/commands.go
@@ -1,14 +1,1 @@
-package cli
 
-import "fmt"
-
-type Globals struct {
-	Config string `help:"config file to use" default:"./config/config.yaml"`
-}
-
-type StateCmd struct{}
-
-func (e *StateCmd) Run(g *Globals) error {
-	fmt.Println("Hello world")
-	return nil
-}
